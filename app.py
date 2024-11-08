09d551695da00368bce805d1b7ca5d0eea94a282
PakDanan <pdanan@gmail.com>
2024-11-08 22:01:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class RecommenderSystem:
         self.all_movies = []
         self.model = None
 
-    def load_data_pickle(self, data_path):
+    def load_data(self, data_path):
         """Load the dataset from a pickle file."""
         with open(data_path, 'rb') as f:
             self.df = pickle.load(f)
@@ -27,10 +27,10 @@ class RecommenderSystem:
             return pd.DataFrame()
 
         rated = self.df[self.df.userId == user_id].movie
-        not_rated = [movie for movie in self.all_movies if movie not in rated]
-        score = [self.model.predict(user_id, movie).est for movie in not_rated]
+        not_rated = [movieId for movieId in self.all_movies if movieId not in rated]
+        score = [self.model.predict(user_id, movieId).est for movieId in not_rated]
 
-        result = pd.DataFrame({"movie": not_rated, "pred_score": score})
+        result = pd.DataFrame({"movieId": not_rated, "pred_score": score})
         result.sort_values("pred_score", ascending=False, inplace=True)
         return result.head(topk)
 
@@ -40,15 +40,21 @@ recommender = RecommenderSystem()
 # Load the model and data pickle files
 data_path = "recommender_data.pkl"  # Path to dataset pickle file
 model_path = "recommender_model.pkl"  # Path to model pickle file
+title_path = "movie_title.pkl"  # Path to model pickle file
 
 @st.cache_data
 def load_data():
-    recommender.load_data_pickle(data_path)
+    recommender.load_data(data_path)
 
 @st.cache_resource
 def load_model():
     recommender.load_model(model_path)
 
+@st.cache_resource
+def load_title():
+    with open(title_path, 'rb') as f:
+        title = pickle.load(f)
+    
 # Streamlit UI
 st.title("Movie Recommender System")
 st.write("Enter a user ID to get personalized movie recommendations.")
@@ -56,6 +62,7 @@ st.write("Enter a user ID to get personalized movie recommendations.")
 # Load the model and data only once
 load_data()
 load_model()
+load_title()
 
 # User input for user ID and top-k recommendations
 user_id = st.number_input("User ID", min_value=1, step=1)
@@ -63,9 +70,10 @@ topk = st.slider("Number of Recommendations", min_value=1, max_value=20, value=1
 
 # Button to generate recommendations
 if st.button("Get Recommendations"):
-    recommendations = recommender.recommend(user_id=int(user_id), topk=topk)
-    if not recommendations.empty:
+    recs = recommender.recommend(user_id=int(user_id), topk=topk)
+    recs=recs.merge(movie,on='movieId')
+    if not recs.empty:
         st.write("Top Recommendations:")
-        st.dataframe(recommendations)
+        st.dataframe(recs)
     else:
         st.write("No recommendations available for this user.")
